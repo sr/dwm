39bedfcee1cf7b79b952be99b98a6d1f2c20922a
Aario Shahbany <aario@users.noreply.github.com>
2016-03-06 17:55:01 +0100

    Added autostart patch

--- dwm.c
+++ dwm.c
@@ -226,6 +226,7 @@ static void resizemouse(const Arg *arg);
 static void resizerequest(XEvent *e);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Window w, Atom proto, int m, long d0, long d1, long d2, long d3, long d4);
 static void sendmon(Client *c, Monitor *m);
@@ -1566,6 +1567,12 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	system("cd ~/.dwm; ./autostart_blocking.sh");
+	system("cd ~/.dwm; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2484,6 +2491,7 @@ main(int argc, char *argv[])
 	setup();
 	scan();
 	run();
+	runAutostart();
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
